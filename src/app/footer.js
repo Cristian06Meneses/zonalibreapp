579eae45e1afc0af74726129655f6cf8e7ed62a2
Cristian06Meneses <cristianmeneses979@gmail.com>
2025-02-01 02:13:01 -0500

    Correccion de errores para deploy

--- src/app/footer.js
+++ src/app/footer.js
@@ -1,3 +1,5 @@
+"use client";
+
 import Link from "next/link";
 import styles from "./styles/footer.module.css";
 import { inter_tight } from "./fonts";
--- src/app/productos/carousel.js
+++ src/app/productos/carousel.js
@@ -1,3 +1,5 @@
+"use client";
+
 import { useEffect, useRef } from "react";
 import styles from "../styles/Carousel.module.css";
 
