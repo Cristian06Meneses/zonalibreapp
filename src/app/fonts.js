1af823d0ec98d92fa21f946315003f7ed6ce395d
Cristian06Meneses <cristianmeneses979@gmail.com>
2025-02-01 02:04:40 -0500

    Coreccion de errores para deploy

--- src/app/fonts.js
+++ src/app/fonts.js
@@ -5,10 +5,10 @@ import { Be_Vietnam_Pro } from 'next/font/google'
 
 
 
-export const abril_fatface = Abril_Fatface({ subsets: ['Latin'], weight: '400' }) // No se usa
+export const abril_fatface = Abril_Fatface({ subsets: ['latin'], weight: '400' }) // No se usa
 
-export const alfa_slab_one = Alfa_Slab_One({ subsets: ['Latin'], weight: '400' }) // No se usa
+export const alfa_slab_one = Alfa_Slab_One({ subsets: ['latin'], weight: '400' }) // No se usa
 
-export const inter_tight = Inter_Tight({ subsets: ['Latin'], weight: '400' })
+export const inter_tight = Inter_Tight({ subsets: ['latin'], weight: '400' })
 
-export const be_vietnam_pro = Be_Vietnam_Pro({ subsets: ['Latin'], weight: '400' })
+export const be_vietnam_pro = Be_Vietnam_Pro({ subsets: ['latin'], weight: '400' })
